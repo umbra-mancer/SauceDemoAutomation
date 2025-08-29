f9e27d29b67ffb0b1e81f68490fd158a762deec6
umbra-mancer <darklyfalse@tutamail.com>
2025-08-29 23:05:59 +0530

    Updating password credentials sequrity using GitHub Secrets

--- src/test/java/com/saucedemo/tests/LoginTest.java
+++ src/test/java/com/saucedemo/tests/LoginTest.java
@@ -2,14 +2,30 @@ package com.saucedemo.tests;
 
 
 import com.saucedemo.base.DriverFactory;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 public class LoginTest {
+
     WebDriver driver;
+    private String username;
+    private String password;
+    @BeforeClass
+    public void loadCredentials() {
+        username = System.getenv("SAUCE_USERNAME");
+        password = System.getenv("sauce_password");
+        if (username == null || password == null) {
+            Dotenv dotenv = Dotenv.load();
+            username = dotenv.get("SAUCE_USERNAME");
+            password = dotenv.get("SAUCE_PASSWORD");
+        }
+    }
 
     @BeforeMethod
     public void setup() {
@@ -19,8 +35,8 @@ public class LoginTest {
 
     @Test
     public void testValidLogin() {
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("login-button")).click();
         assert driver.getCurrentUrl().contains("inventory");
     }
